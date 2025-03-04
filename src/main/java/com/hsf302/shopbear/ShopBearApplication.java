f922c5adae3c2679e14bf272efb2f31dc01d1e88
Huy Tran <huytran@MacBookPro.lan>
2025-03-04 20:46:34 +0700

    set up

---
+++ src/main/java/com/hsf302/shopbear/ShopBearApplication.java
@@ -0,0 +1,13 @@
+package com.hsf302.shopbear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopBearApplication.class, args);
+    }
+
+}
